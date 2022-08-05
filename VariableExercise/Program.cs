e325e9f5a29a11229976579271e90bd043e2962b
LaKiesha Hall <LaKiesha.Hall@Gmail.Com>
2022-08-05 14:47:38 -0400

    initial commit

---
+++ VariableExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariableExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int dogAge = 8; 
+            string dogName = "Titanic"; 
+            bool isPureBread = true;
+            char nickName = 'T';
+            double bones = 5;
+            decimal weight = 40.7m;
+            
+
+            Console.WriteLine($"My dog's name is {dogName}, {nickName} for short. He is {dogAge} years old and weighs {weight} pounds. If you heard me state he's a pure bread Huskie, it's {isPureBread}! He's burried {bones} bones in our neighbor's backyard.");
+
+            
+        }
+    }
+}
